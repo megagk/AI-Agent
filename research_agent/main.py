3c437bb46c4726292414ba4e51eb3c08b4aed0ee
Amegag <megag.ahmed@gmail.com>
2025-03-24 22:16:18 +1100

    restructure

--- research_agent/main.py
+++ research_agent/main.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from tools import search_tool, wiki_tool, save_tool
+from research_agent.tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
